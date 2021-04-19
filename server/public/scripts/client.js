9b13e7bcf78deacdb4da5eb1ff753d39551b17a2
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-04-19 11:42:29 -0500

    updated client.js file

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,33 @@ $( document ).ready( onReady );
 
 function onReady() {
     console.log('DOM ready');
+
+    $('#addJokeButton').on('click', function(event) {
+        console.log('clicked');
+
+        event.preventDefault();
+        addJoke();
+    })
+}
+
+function addJoke() {
+    let newJoke = {
+        whoseJoke: $('#whoseJokeIn').val(),
+        jokesQuestion: $('#questionIn').val(),
+        punchLine: $('#punchLineIn').val(),
+    }
+    console.log('Adding joke', newJoke);
+
+    $.ajax({
+        method: 'POST',
+        url: '/jokes',
+        data: newJoke
+    })
+        .then(function(response){
+            console.log('Added a joke');
+        })
+        .catch(function(error){
+            console.log('Error from server', error);
+            alert('Sorry, could not add your joke. Try again.')
+        })
 }
